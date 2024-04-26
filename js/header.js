90b2cc98c157b87fba04327d1ffee9c866f90318
Rocío Palagano <rocio.palagano97@gmail.com>
2024-04-26 15:50:22 -0300

    Inicio Prueba

---
+++ js/header.js
@@ -0,0 +1,14 @@
+const nav = document.getElementById("menu");
+const abrir = document.getElementById("abrir");
+const cerrar = document.getElementById("cerrar");
+
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
+
+
